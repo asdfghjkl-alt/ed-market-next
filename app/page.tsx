bcfb454e8262a0f7fa519f1c2bee97159e466d7f
Edward Wenkai Liu <liuedward180@gmail.com>
2026-01-30 18:31:19 +1100

    Constructed the home page, defined fonts and added favicon and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,56 @@
+// import Github from "@/assets/Github.png";
+// import LinkedIn from "@/assets/LinkedIn.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="animate-fade-in-up flex min-h-screen flex-col items-center justify-center p-5 text-center">
+      <Image src="/EdMarket.png" alt="Logo" height={200} width={200} />
+      <h1 className="mt-4">Welcome to EdMarket!</h1>
+      <p className="mt-4 text-xl">
+        Your one-stop shop for all your grocery needs.
+      </p>
+      <p className="mt-4 max-w-2xl sm:mx-5 md:mx-30">
+        A full-stack e-commerce application built with MERN stack (MongoDB,
+        Express, React, Node.js) by Edward Liu.
+      </p>
+      <p className="mt-4 max-w-2xl sm:mx-5 md:mx-30">
+        The project constructs a full shopping website experience featuring
+        secure user authentication, an admin dashboard for product, category and
+        order management, dynamic cart display, and order processing.
+      </p>
+      <p className="mt-4 max-w-2xl sm:mx-5 md:mx-30">
+        Further, Role Based Access Control has now been fully implemented with
+        roles of buyers, sellers and admins with different level of permissions
+        for actions.
+      </p>
+      <p className="mt-4 max-w-2xl sm:mx-5 md:mx-30">
+        In the project, buyers can only view products and add them to cart,
+        sellers can also create, update and delete their own products, and
+        admins can delete any products, manage orders, manage user roles and add
+        product categories.
+      </p>
+      <Link
+        href="/products"
+        className="btn mt-4 flex items-center bg-blue-600 p-3 text-blue-100"
+      >
+        Browse Products
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/edward-liu-50a205267"
+        className="btn mt-4 flex items-center"
+      >
+        <Image src="/LinkedIn.png" alt="LinkedIn" height={50} width={50} />
+        <p>View Owner's LinkedIn</p>
+      </Link>
+      <Link
+        href="https://github.com/asdfghjkl-alt/EdMarket"
+        className="btn mt-4 flex items-center"
+      >
+        <Image src="/Github.png" alt="Github" height={50} width={50} />
+        <p>View on GitHub</p>
+      </Link>
     </div>
   );
 }
