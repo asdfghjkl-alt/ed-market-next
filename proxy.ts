a54b4062e5ca1f1e5791b40bcb498754074c13f9
Edward Wenkai Liu <liuedward180@gmail.com>
2026-02-01 20:44:00 +1100

    Added strict-dynamic

--- proxy.ts
+++ proxy.ts
@@ -9,7 +9,7 @@ export function proxy(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ""};
-    style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`};
+    style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}' 'strict dynamic'`};
     img-src 'self' blob: data: https://res.cloudinary.com;
     font-src 'self';
     object-src 'none';
