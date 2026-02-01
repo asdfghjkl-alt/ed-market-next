6ddd669134731e5126e35513b1a5f3188e61bf72
Edward Wenkai Liu <liuedward180@gmail.com>
2026-02-01 20:36:18 +1100

    Applied csp header subjectively for is dev or not is dev

--- proxy.ts
+++ proxy.ts
@@ -4,13 +4,13 @@ import type { NextRequest } from "next/server";
 export function proxy(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
-  const isProduction = process.env.NODE_ENV === "production";
+  const isDev = process.env.NODE_ENV === "development";
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data: https://res.cloudinary.com;
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ""};
+    style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`};
+    img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
@@ -32,12 +32,10 @@ export function proxy(request: NextRequest) {
     },
   });
 
-  if (isProduction) {
-    response.headers.set(
-      "Content-Security-Policy",
-      cspHeader.replace(/\s{2,}/g, " ").trim(),
-    );
-  }
+  response.headers.set(
+    "Content-Security-Policy",
+    cspHeader.replace(/\s{2,}/g, " ").trim(),
+  );
 
   return response;
 }
